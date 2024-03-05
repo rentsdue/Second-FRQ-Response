4ede328f866f4920d0052acfd54568d2a536281e
rentsdue <hoganwong88@gmail.com>
2024-03-05 13:07:20 +0800

    done

--- LightBoard.java
+++ LightBoard.java
@@ -1,9 +1,9 @@
 public class LightBoard {
 
 /* The lights on the board, where true represents on and false represents off.*/
-private boolean[][] lights;
+    private boolean[][] lights;
 
-/** Constructs a LightBoard object having numRows rows and numCols columns.
+/* Constructs a LightBoard object having numRows rows and numCols columns.
 Precondition: numRows > 0, numCols > 0
 Postcondition: each light has a 40% probability of being set to on.
 */
